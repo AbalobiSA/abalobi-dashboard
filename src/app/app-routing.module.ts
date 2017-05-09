7cc50a1af762e5b7e8ad05ad9540bb082cf48746
Carl Eiserman <carleiserman@gmail.com>
2017-05-09 14:50:22 +0100

    feature: linked all new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,33 +5,43 @@ import { RouterModule, Routes } from '@angular/router';
 // import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './partials/navbar/navbar.component';
+import { ToolsComponent } from './components/tools/tools.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 const routes: Routes = [
-  {
-    // Default path
-    path: '',
-    redirectTo: 'home/',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  }
+    {
+        // Default path
+        path: '',
+        redirectTo: 'home/',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'search',
+        component: SearchComponent
+    },
+    {
+        path: 'tools',
+        component: ToolsComponent
+    }
 ];
 
 // Use hash in location routes, for hosting on heroku
 const routeSettings = {
-  useHash: true
+    useHash: true
 };
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes, routeSettings)
-  ],
-  declarations: [  ],
-  exports: [ RouterModule ]
+    imports: [
+        CommonModule,
+        RouterModule.forRoot(routes, routeSettings)
+    ],
+    declarations: [  ],
+    exports: [ RouterModule ]
 })
 
 export class AppRoutingModule { }
